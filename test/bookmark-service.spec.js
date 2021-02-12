7b66609101ceef7ae3a8a150f49a16861e4702e4
Nishat Hoque <hoquen0397@gmail.com>
2021-02-12 17:00:58 -0500

    bookmarks.fixtures

--- test/bookmark-service.spec.js
+++ test/bookmark-service.spec.js
@@ -4,32 +4,11 @@ require('dotenv').config();
 const { expect } = require('chai');
 const knex = require('knex');
 const BookmarkService = require('../src/bookmark/bookmark-service');
+const makeBookmarksArray = require('./bookmarks.fixtures');
 
 describe('Bookmarks service object', function(){
     let db;
-    let testBookmarks = [
-        {   
-            id: 1,
-            title: 'Thinkful',
-            url: 'https://www.thinkful.com',
-            description: 'Think outside the classroom',
-            rating: '5' 
-        },
-        { 
-            id: 2,
-            title: 'Google',
-            url: 'https://www.google.com',
-            description: 'Where we find everything else',
-            rating: '4' 
-        },
-        { 
-            id: 3,
-            title: 'MDN',
-            url: 'https://developer.mozilla.org',
-            description: 'The only place to find web documentation',
-            rating: '5' 
-        },
-    ];
+    let testBookmarks = makeBookmarksArray();
 
     before(() => {
         db = knex({
