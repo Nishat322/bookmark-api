321fcd7a05cd70034b5e97a1b722b8c6d1498db9
Nishat Hoque <hoquen0397@gmail.com>
2021-02-12 01:32:40 -0500

    seed the data, readme update

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,10 @@
+INSERT INTO bookmarks (title, url, description, rating)
+VALUES
+    ('New York Times',  'https://www.nytimes.com/',         'News paper online',            '3'),
+    ('Google',          'https://www.google.com',           'Search Engine',                '5'),
+    ('Yahoo',           'https://www.yahoo.com',            'Search Engine',                '4'),
+    ('GitHub',          'https://github.com',               'Contain respositories',        '5'),
+    ('Bing',            'https://bing.com',                 'Really old Search Engine',     '2'),
+    ('Youtube',         'https://youtube.com',              'Video Search Engine',          '3'),
+    ('Thinkful',        'https://www.thinkful.com',         'Think outside the classroom',  '4'),
+    ('MDN',             'https://developer.mozilla.org',    'Web documentation',            '1');
